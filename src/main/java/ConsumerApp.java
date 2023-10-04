0f076604102309bb27e996b0341abbf8bed7a190
urawat_meshiq <urawat@meshiq.com>
2023-10-04 22:30:33 +0530

    Updated the files with jaeger tracing

--- src/main/java/ConsumerApp.java
+++ src/main/java/ConsumerApp.java
@@ -1,6 +1,19 @@
 
 import com.solacesystems.jcsmp.*;
 import com.solacesystems.jcsmp.JCSMPStreamingPublishEventHandler;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.api.trace.TracerBuilder;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.jaeger.JaegerGrpcSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
@@ -10,6 +23,12 @@ import java.time.Duration;
 import java.util.*;
 
 public class ConsumerApp {
+    private static Tracer tracer;
+
+    public ConsumerApp(OpenTelemetry openTelemetry){
+        tracer = openTelemetry.getTracer("io.opentelemetry.example.JaegerExample");
+    }
+
     public static void main(String[] args) {
 
         final Logger logger = LoggerFactory.getLogger(ConsumerApp.class);
@@ -39,6 +58,24 @@ public class ConsumerApp {
         solaceProps.setProperty(JCSMPProperties.USERNAME, solaceUsername);
         solaceProps.setProperty(JCSMPProperties.PASSWORD, solacePassword);
 
+        //Initialize the Jaeger exporter
+        JaegerGrpcSpanExporter jaegerExporter = JaegerGrpcSpanExporter.builder()
+                .setEndpoint("http://jaeger-collector:14268/api/traces")
+                .build();
+
+        //Create an OpenTelemetry Tracer
+        SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
+                .addSpanProcessor(BatchSpanProcessor.builder(jaegerExporter).build())
+                .build();
+
+        //Create an OpenTelemetry instance
+        io.opentelemetry.api.OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
+                .setTracerProvider(tracerProvider)
+                .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+                .build();
+        //Set the global OpenTelemetry instance
+        GlobalOpenTelemetry.set(openTelemetry);
+
         XMLMessageProducer solaceProducer = null;
         try {
             JCSMPSession solaceSession = JCSMPFactory.onlyInstance().createSession(solaceProps);
@@ -62,13 +99,31 @@ public class ConsumerApp {
                     // Extract Kafka message
                     String kafkaMessage = record.value();
 
-                    // Add context information
-                    String solaceMessage = ContextPropagator.addContext(kafkaMessage);
+                    //Create a span for Kafka processing
+                    Span kafkaSpan = tracer.spanBuilder("Kafka Processing").startSpan();
+                    try (Scope scope = kafkaSpan.makeCurrent()){
+                        //Add attributes to the Kafka span
+                        kafkaSpan.setAttribute("kafkatest",kafkaTopic);
 
-                    // Publish to Solace
-                    TextMessage solaceTextMsg = JCSMPFactory.onlyInstance().createMessage(TextMessage.class);
-                    solaceTextMsg.setText(solaceMessage);
-                    solaceProducer.send(solaceTextMsg, JCSMPFactory.onlyInstance().createTopic(solaceTopic));
+                        //Add context information
+                        String solaceMessage = ContextPropagator.addContext(kafkaMessage);
+
+                        //Create a span for Solace processing
+                        Span solaceSpan = tracer.spanBuilder("Solace Processing").startSpan();
+                        try (Scope solaceScope = solaceSpan.makeCurrent()) {
+                            //Add attributes to the Solace span
+                            solaceSpan.setAttribute("solacetest", solaceTopic);
+
+                            //Publish to Solace
+                            TextMessage solaceTextMsg = JCSMPFactory.onlyInstance().createMessage(TextMessage.class);
+                            solaceTextMsg.setText(solaceMessage);
+                            solaceProducer.send(solaceTextMsg, JCSMPFactory.onlyInstance().createTopic(solaceTopic));
+                        } finally {
+                            solaceSpan.end();
+                        }
+                    }finally {
+                        kafkaSpan.end();
+                    }
                 }
             }
         } catch (Exception e) {
